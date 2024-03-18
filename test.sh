ca1c8d84b967e10999c2744ae23d3e9feda480bd
Arjun Kunna <arjunk1@sapling2.stanford.edu>
2024-03-18 16:36:23 -0700

    removed source env.sh from test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
-export INCLUDE_PATH="$INCLUDE_PATH;/home/arjunk1/regent-fft-arjun/fftw-3.3.8/install/include"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/arjunk1/regent-fft-arjun/fftw-3.3.8/install/lib"
-export TERRA_PATH="$TERRA_PATH;/home/arjunk1/regent-fft-arjun/src/?.rg"
+export INCLUDE_PATH="$INCLUDE_PATH;$PWD/fftw-3.3.8/install/include"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PWD/fftw-3.3.8/install/lib"
+export TERRA_PATH="$TERRA_PATH;$PWD/src/?.rg"
 git clone https://github.com/StanfordLegion/legion.git
 CC=gcc CXX=g++ DEBUG=1 USE_GASNET=0 ./legion/language/scripts/setup_env.py
 ./install.py
