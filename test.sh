3cfafd7a00a3c5977a12a767d599e724ec9759c4
Arjun Kunna <arjunk1@g0003.stanford.edu>
2024-03-18 15:28:40 -0700

    test

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
+export INCLUDE_PATH="$INCLUDE_PATH;/home/arjunk1/regent-fft-arjun/fftw-3.3.8/install/include"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/arjunk1/regent-fft-arjun/fftw-3.3.8/install/lib"
+export TERRA_PATH="$TERRA_PATH;/home/arjunk1/regent-fft-arjun/src/?.rg"
+source env.sh
 git clone https://github.com/StanfordLegion/legion.git
-cd legion/language
-./install.py --rdir=auto --debug
-cd ../..
-legion/language/regent.py fft_test.rg
+CC=gcc CXX=g++ DEBUG=1 USE_GASNET=0 ./legion/language/scripts/setup_env.py
+./install.py
+./legion/language/regent.py test/fft_test.rg
