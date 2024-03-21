5af9c3244a82eb003c6d7d7de5adebaaf513b5ed
Arjun Kunna <arjunk1@sapling2.stanford.edu>
2024-03-21 14:59:27 -0700

    cuda path

--- test.sh
+++ test.sh
@@ -2,11 +2,10 @@ export INCLUDE_PATH="$INCLUDE_PATH;$PWD/fftw-3.3.8/install/include"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PWD/fftw-3.3.8/install/lib"
 export TERRA_PATH="$TERRA_PATH;$PWD/src/?.rg"
 
-export CUDA_PATH=/usr/local/cuda
-export CUDA=/usr/local/cuda
+export CUDA_PATH=$PWD/usr/local/cuda
+export CUDA=$PWD/usr/local/cuda
 
 echo "CUDA_PATH=${CUDA_PATH}"
-export CUDA_PATH=${CUDA_PATH}
 export PATH="$CUDA_PATH/bin:$PATH"
 export LD_LIBRARY_PATH="$CUDA_PATH/lib:$LD_LIBRARY_PATH"
 export LD_LIBRARY_PATH="$CUDA_PATH/lib64:$LD_LIBRARY_PATH"
@@ -21,8 +20,6 @@ if [[ $GITHUB_ACTIONS ]]; then
     echo "LD_LIBRARY_PATH=${CUDA_PATH}/lib:${LD_LIBRARY_PATH}" >> $GITHUB_ENV
     echo "LD_LIBRARY_PATH=${CUDA_PATH}/lib64:${LD_LIBRARY_PATH}" >> $GITHUB_ENV
 fi
-
-export CUDA=${CUDA_PATH}
 
 git clone https://github.com/StanfordLegion/legion.git
 CC=gcc CXX=g++ DEBUG=1 USE_GASNET=0 ./legion/language/scripts/setup_env.py
