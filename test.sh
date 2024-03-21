6ce68cde98655c81c1489b8634193f09ee684251
Arjun Kunna <arjunk1@sapling2.stanford.edu>
2024-03-21 09:11:35 -0700

    test

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 export INCLUDE_PATH="$INCLUDE_PATH;$PWD/fftw-3.3.8/install/include"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PWD/fftw-3.3.8/install/lib"
 export TERRA_PATH="$TERRA_PATH;$PWD/src/?.rg"
+export INCLUDE_PATH="$PWD/../src"
+export LIB_PATH="$PWD/src"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PWD/src"
 git clone https://github.com/StanfordLegion/legion.git
 CC=gcc CXX=g++ DEBUG=1 USE_GASNET=0 ./legion/language/scripts/setup_env.py
-./install.py
-./legion/language/regent.py test/fft_test.rg
+
