47f4952d721cf588ed113f03a41bfae8c8ee396a
Arjun Kunna <arjunk1@sapling2.stanford.edu>
2024-03-21 08:41:16 -0700

    test

--- install_cuda.sh
+++ install_cuda.sh
@@ -3,15 +3,11 @@
 set -e
 set -x
 
-sudo_command="$1"
-
-sudo apt-get update -qq
-
-$sudo_command apt-get update -qq
-$sudo_command apt-get install -qq software-properties-common
+$SUDO_COMMAND apt-get update -qq
+$SUDO_COMMAND apt-get install -qq software-properties-common
 wget -nv https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-$sudo_command mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-$sudo_command apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/3bf863cc.pub
-$sudo_command add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
-$sudo_command apt-get update -qq
-$sudo_command apt-get install -qq cuda-compiler-11.6
+$SUDO_COMMAND mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+$SUDO_COMMAND apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/3bf863cc.pub
+$SUDO_COMMAND add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+$SUDO_COMMAND apt-get update -qq
+$SUDO_COMMAND apt-get install -qq cuda-compiler-11.6
