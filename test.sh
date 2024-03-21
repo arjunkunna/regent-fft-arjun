37035cebfd2f442d79d670e39ef879275affafb4
Arjun Kunna <arjunk1@sapling2.stanford.edu>
2024-03-21 10:18:35 -0700

    testing path

--- test.sh
+++ test.sh
@@ -1,9 +1,6 @@
-export INCLUDE_PATH="$INCLUDE_PATH;$PWD/fftw-3.3.8/install/include"
+export INCLUDE_PATH="$INCLUDE_PATH:$PWD/fftw-3.3.8/install/include"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PWD/fftw-3.3.8/install/lib"
-export TERRA_PATH="$TERRA_PATH;$PWD/src/?.rg"
-export INCLUDE_PATH="$INCLUDE_PATH:${CUDA_HOME}/include"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${CUDA_HOME}/lib64"
-
+export TERRA_PATH="$TERRA_PATH:$PWD/src/?.rg"
 git clone https://github.com/StanfordLegion/legion.git
 CC=gcc CXX=g++ DEBUG=1 USE_GASNET=0 ./legion/language/scripts/setup_env.py
 
