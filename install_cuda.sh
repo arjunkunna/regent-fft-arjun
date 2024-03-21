75f514b83f1a290eee7fac3a1427dc470fe3fdf3
Arjun Kunna <arjunk1@sapling2.stanford.edu>
2024-03-21 15:33:36 -0700

    moved install cuda into test

--- install_cuda.sh
+++ install_cuda.sh
@@ -11,25 +11,3 @@ sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda
 sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
 sudo apt-get update -qq
 sudo apt-get install -qq cuda-compiler-11.6
-
-export CUDA_PATH=/usr/local/cuda
-export CUDA=/usr/local/cuda
-
-echo "CUDA_PATH=${CUDA_PATH}"
-export CUDA_PATH=${CUDA_PATH}
-export PATH="$CUDA_PATH/bin:$PATH"
-export LD_LIBRARY_PATH="$CUDA_PATH/lib:$LD_LIBRARY_PATH"
-export LD_LIBRARY_PATH="$CUDA_PATH/lib64:$LD_LIBRARY_PATH"
-
-
-# If executed on github actions, make the appropriate echo statements to update the environment
-if [[ $GITHUB_ACTIONS ]]; then
-    # Set paths for subsequent steps, using ${CUDA_PATH}
-    echo "Adding CUDA to CUDA_PATH, PATH and LD_LIBRARY_PATH"
-    echo "CUDA_PATH=${CUDA_PATH}" >> $GITHUB_ENV
-    echo "${CUDA_PATH}/bin" >> $GITHUB_PATH
-    echo "LD_LIBRARY_PATH=${CUDA_PATH}/lib:${LD_LIBRARY_PATH}" >> $GITHUB_ENV
-    echo "LD_LIBRARY_PATH=${CUDA_PATH}/lib64:${LD_LIBRARY_PATH}" >> $GITHUB_ENV
-fi
-
-export CUDA=${CUDA_PATH}
--- test.sh
+++ test.sh
@@ -2,6 +2,21 @@ export INCLUDE_PATH="$INCLUDE_PATH;$PWD/fftw-3.3.8/install/include"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PWD/fftw-3.3.8/install/lib"
 export TERRA_PATH="$TERRA_PATH;$PWD/src/?.rg"
 
+#!/bin/bash
+
+set -e
+set -x
+
+sudo apt-get update -qq
+sudo apt-get install -qq software-properties-common
+wget -nv https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/3bf863cc.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+sudo apt-get update -qq
+sudo apt-get install -qq cuda-compiler-11.6
+
+
 export CUDA_PATH=/usr/local/cuda
 export CUDA=/usr/local/cuda
 
