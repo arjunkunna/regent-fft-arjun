889e03aa1954ed021c2fb20b6a2eaf82c6968547
Arjun Kunna <arjunk1@sapling2.stanford.edu>
2024-03-22 16:47:14 -0700

    new commands

--- install_cuda.sh
+++ install_cuda.sh
@@ -10,4 +10,4 @@ sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/3bf863cc.pub
 sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
 sudo apt-get update -qq
-sudo apt-get install -qq cuda-compiler-11.6
+sudo apt-get install -qq cuda-toolkit-11.6
