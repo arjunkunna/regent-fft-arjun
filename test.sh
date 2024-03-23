6b492af40eeaa2ef93e0c241129c034eef8be269
Arjun Kunna <arjunk1@sapling2.stanford.edu>
2024-03-22 17:41:15 -0700

    new commands

--- test.sh
+++ test.sh
@@ -9,15 +9,16 @@ set -x
 
 sudo apt-get update -qq
 sudo apt-get install -qq software-properties-common
-apt-cache depends cuda-toolkit-11-6
-dpkg --get-selections | grep hold
+
 sudo apt-get clean
 sudo apt-get autoclean
 
-wget -nv https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+wget -nv https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
 sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/3bf863cc.pub
-sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/3bf863cc.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/ /"
+
+
 sudo apt-get update -qq
 sudo apt-get install -qq cuda-toolkit-11.6
 
