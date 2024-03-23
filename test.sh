83685f2c7c57a908973d2c84840ca77e57468c45
Arjun Kunna <arjunk1@sapling2.stanford.edu>
2024-03-22 17:21:06 -0700

    new commands

--- test.sh
+++ test.sh
@@ -7,14 +7,14 @@ export TERRA_PATH="$TERRA_PATH;$PWD/src/?.rg"
 set -e
 set -x
 
-#sudo apt-get update -qq
-#sudo apt-get install -qq software-properties-common
-#wget -nv https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-#sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-#sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/3bf863cc.pub
-#sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
-#sudo apt-get update -qq
-#sudo apt-get install -qq cuda-toolkit-11.6
+sudo apt-get update -qq
+sudo apt-get install -qq software-properties-common
+wget -nv https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/3bf863cc.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+sudo apt-get update -qq
+sudo apt-get install -qq cuda-toolkit-11.6
 
 export INCLUDE_PATH="$INCLUDE_PATH;$PWD/usr/local/cuda/include"
 
